64df752bed6476355abd9ea944d533fe84f6d8b1
Gabriel Lemos <jmjgb@outlook.com>
2023-01-12 13:16:08 -0300

    Requisito 5 finalizado

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    artist: '',
+  };
+
   render() {
+    const { artist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            onChange={ (evt) => this.setState({ artist: evt.target.value }) }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            onClick={ (evt) => evt.preventDefault() }
+            disabled={ artist.length < 2 }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
